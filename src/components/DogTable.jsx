99b42192e0a7e4749c333972aeb78a7f478f34ce
Shivam Rana <shivam105228@gmail.com>
2024-06-07 10:13:33 +0530

    Update DogTable.jsx

--- src/components/DogTable.jsx
+++ src/components/DogTable.jsx
@@ -38,7 +38,7 @@ const DogTable = () => {
             "Content-Type": "application/json",
             // API key for The Dog API (replace with your actual API key)
             "x-api-key":
-              "YOUR_API_KEY",
+              "live_tZuLOgaciL37gw3UwEEuoFtFKx8GX7Ep9wgPjTZlv7da0Hdd1mEwzU0NtvMlkLTb",
           },
         }
       );
